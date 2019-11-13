b566b39322fd7853e74e737e9446abd331c4618b
maxgdev <max@apphilia.net>
2019-11-13 14:21:36 +0000

    unique placeholder images for 3x2 flex home page & menu tyling

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
 import React from 'react';
 import './App.css';
 import aboutMe from './images/aboutme.jpg';
+import fruitCake from './images/fruitcake.jpg';
 import projects from './images/projects.jpg';
+import codeScreen from './images/codescreen.jpg';
+import keyboardMouse from './images/keyboardmouse.jpg';
+import macLaptop from './images/maclaptop.jpg';
+import tablet from './images/tablet.jpg';
+
 function App() {
   return (
     <div className="App">
       <nav>
         <ul class="menu">
-          <li class="logo"><a href="#">MaxGDev</a></li>
-          <li class="item"><a href="#">Home</a></li>
-          <li class="item"><a href="#">About</a></li>
-          <li class="item"><a href="#">Resume</a></li>
-          <li class="item"><a href="#">Projects</a></li>
-          <li class="item"><a href="#">Blog</a></li>
-          <li class="item"><a href="#">Contact</a></li>
+          <li class="logo"><a href="a">MaxGDev</a></li>
+          <li class="item"><a href="a">Home</a></li>
+          <li class="item"><a href="a">About</a></li>
+          <li class="item"><a href="a">Resume</a></li>
+          <li class="item"><a href="a">Projects</a></li>
+          <li class="item"><a href="a">Blog</a></li>
+          <li class="item"><a href="a">Contact</a></li>
         </ul>
       </nav>
       <header className="header">
 
       </header>
       <main>
-        <section>
+        <section className='sectionContainer'>
           <div className='sectionItem'>
-            <img src={aboutMe} alt='about me' />
+            <img src={fruitCake} alt='about me' />
             <div className='overlay'></div>
           </div>
           <div className='sectionItem'>
-            <img src={aboutMe} alt='about me' />
+            <img src={macLaptop} alt='lorem ipsum' />
             <div className='overlay'></div>
           </div>
           <div className='sectionItem'>
-            <img src={aboutMe} alt='about me' />
+            <img src={keyboardMouse} alt='lorem ipsum' />
             <div className='overlay'></div>
           </div>
         </section>
-        <section>
+        <section  className='sectionContainer'>
           <div className='sectionItem'>
             <img src={projects} alt='projects' />
             <div className='overlay'></div>
           </div>
           <div className='sectionItem'>
-            <img src={projects} alt='projects' />
+            <img src={codeScreen} alt='lorem ipsum' />
             <div className='overlay'></div>
           </div>
           <div className='sectionItem'>
-            <img src={projects} alt='projects' />
+            <img src={tablet} alt='lorem ipsum' />
             <div className='overlay'></div>
           </div>
         </section>  
