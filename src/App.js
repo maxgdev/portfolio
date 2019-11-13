c5363c958331af2fa838a251ae2858d19b9e7a2a
maxgdev <max@apphilia.net>
2019-11-13 15:15:50 +0000

    added text overlays to grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import aboutMe from './images/aboutme.jpg';
 import fruitCake from './images/fruitcake.jpg';
 import projects from './images/projects.jpg';
 import codeScreen from './images/codescreen.jpg';
@@ -22,39 +21,39 @@ function App() {
           <li class="item"><a href="a">Contact</a></li>
         </ul>
       </nav>
-      <header className="header">
-
-      </header>
       <main>
         <section className='sectionContainer'>
           <div className='sectionItem'>
             <img src={fruitCake} alt='about me' />
-            <div className='overlay'></div>
+            <div className='overlay'>About Me</div>
           </div>
           <div className='sectionItem'>
             <img src={macLaptop} alt='lorem ipsum' />
-            <div className='overlay'></div>
+            <div className='overlay'>Projects</div>
           </div>
           <div className='sectionItem'>
             <img src={keyboardMouse} alt='lorem ipsum' />
-            <div className='overlay'></div>
+            <div className='overlay'>Resume</div>
           </div>
         </section>
         <section  className='sectionContainer'>
           <div className='sectionItem'>
             <img src={projects} alt='projects' />
-            <div className='overlay'></div>
+            <div className='overlay'>Blog</div>
           </div>
           <div className='sectionItem'>
             <img src={codeScreen} alt='lorem ipsum' />
-            <div className='overlay'></div>
+            <div className='overlay'>Skillset</div>
           </div>
           <div className='sectionItem'>
             <img src={tablet} alt='lorem ipsum' />
-            <div className='overlay'></div>
+            <div className='overlay'>Contact</div>
           </div>
         </section>  
       </main>
+      <footer className=''>
+        <a href='/'>Back to Top</a>
+      </footer>
     </div>
   );
 }
