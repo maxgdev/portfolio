c564985b60b182dc9818f920c1917fb028a4b941
maxgdev <max@apphilia.net>
2019-12-06 00:03:53 +0000

    changed logo a tag to #3bd6c6

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,11 +13,11 @@ export default function Navbar() {
         <div class='navcenter'></div>
         <div class='menublock'>
             <ul class='menu'>
-                <li class='item'><a href='a'>Home</a></li>
+                <li class='#Home'><a href='a'>Home</a></li>
                 <li class='#About'><a href='a'>About</a></li>
-                <li class='item'><a href='a'>Resume</a></li>
+                <li class='#Resume'><a href='a'>Resume</a></li>
                 <li class='#Projects'><a href='a'>Projects</a></li>
-                <li class='item'><a href='http://www.d3jsdataviz.com/' target='_blank' rel='noopener noreferrer'>Blog</a></li>
+                <li class='#Blog'><a href='http://www.d3jsdataviz.com/' target='_blank' rel='noopener noreferrer'>Blog</a></li>
                 <li class='#Contact'><a href='a'>Contact</a></li>
             </ul>
         </div>
